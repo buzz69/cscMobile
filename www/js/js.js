061a86a9e2d616eb76d67b833a165ecd044e8f4b
buzz69 <sebastien.masson.pro@gmail.com>
2013-07-15 16:16:24 +0200

    reretrait blink

--- www/js/js.js
+++ www/js/js.js
@@ -123,7 +123,7 @@
 		}
 		function loadCameras(){
 			//showLoader('Récupération des caméras...');
-			$('#footerDiv').addClass("blink");
+			//$('#footerDiv').addClass("blink");
 			$('#footerDiv').html('<br><center><table><tr><td align=center valign=center width=35><img src="glyphish-icons/55-network.png"></img></td><td align=center valign=center><div id="footerTxt">Chargement en cours...</div></td></tr></table></center></br>');
 			$('#camerasListe').empty().listview("refresh");
 			$('#camerasListe2').empty().listview("refresh");
@@ -135,7 +135,7 @@
 				data       : {action: 'LOAD_CAMERAS'},
 				success    : function(rep) {
 					$('#footerDiv').html('<br><center><table><tr><td align=center valign=center width=35><img src="glyphish-icons/01-refresh.png"></img></td><td align=center valign=center><div id="footerTxt">Rafraichir la liste</div></td></tr></table></center></br>');
-					$('#footerDiv').removeClass("blink");
+					//$('#footerDiv').removeClass("blink");
 					if(rep.status=='ERROR'){
 						alert('Status: '+rep.status+"\nMessage: "+rep.errorMsg);
 					}else{
