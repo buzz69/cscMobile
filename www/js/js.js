877064d8ddf9b63fe809b31c1ca3d72172e1b643
buzz69 <sebastien.masson.pro@gmail.com>
2013-07-16 16:48:30 +0200

    test exit app

--- www/js/js.js
+++ www/js/js.js
@@ -3,13 +3,7 @@
 				$.mobile.allowCrossDomainPages = true;
 				$.mobile.defaultPageTransition = "slide";
 				$.event.special.tap.tapholdThreshold=100;
-				    document.addEventListener("backbutton", function() {
-						if ( $('.ui-page-active').attr('id') == 'camlistPage') {
-							exitAppPopup();
-						} else {
-							location.href='cameras.html';          
-						}
-					}, false);
+				    document.addEventListener("backbutton", function() { alert('EXIT');	}, false);
 		});
 		
 		var play='off';
