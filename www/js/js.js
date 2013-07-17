652d3fa60aee93d051b52ae54f74327fc1c27e0a
buzz69 <sebastien.masson.pro@gmail.com>
2013-07-17 10:00:40 +0200

    Fermeture de l'appli si aucune connexion internet

--- www/js/js.js
+++ www/js/js.js
@@ -42,15 +42,20 @@
 			var networkState = navigator.network.connection.type;
 
 			var states = {};
-			states[Connection.UNKNOWN]  = 'Unknown connection';
+			states[Connection.UNKNOWN]  = 'Unknow connection';
 			states[Connection.ETHERNET] = 'Ethernet connection';
 			states[Connection.WIFI]     = 'WiFi connection';
 			states[Connection.CELL_2G]  = 'Cell 2G connection';
 			states[Connection.CELL_3G]  = 'Cell 3G connection';
 			states[Connection.CELL_4G]  = 'Cell 4G connection';
-			states[Connection.NONE]     = 'No network connection';
-
-			alert('Connection type:\n ' + states[networkState]);
+			states[Connection.NONE]     = 'NONE';
+			
+			if(states[networkState]=='NONE'){
+				alert('Une connexion internet est nécessaire !');
+				exitAPP();
+			}
+			
+			//alert('Connection type:\n ' + states[networkState]);
 		}
 		
 		var play='off';
