1ce08a12f74e6ffffa7fda3809449f4649b426c8
buzz69 <sebastien.masson.pro@gmail.com>
2014-02-26 23:21:32 +0100

    Correction bug canva

--- www/js/js.js
+++ www/js/js.js
@@ -303,10 +303,6 @@
 			setTimeout("$('#popupInfos').popup('open')",500);
 		}
 		
-		if (/android/i.test(navigator.userAgent)) {
-		   //this is for phonegap in android
-		   jQuery('canvas').parent().css('overflow', 'visible');
-		}
 		window.onorientationchange=function(){	//detection du mode paysage
 			wheight = $(window).height();
             changepush();
